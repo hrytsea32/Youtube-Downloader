fd736418157fe26bb437a352d8be13c518f20100
Andrii <amg97@yandex.com>
2023-02-11 04:34:12 -0600

    Updates
    Now, after downloading the video, the sctipt opens its location

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # Here's an easy automated script for downloading YouTube videos.
 # Simply use the code below to download any video without the need for any websites or apps.
-import pytube
+from pytube import YouTube as yt
+from pytube.exceptions import VideoUnavailable
+import subprocess
+import os
 
-def download(link):
-    video_download = pytube.Youtube(link)
-    video_download.streams.first().download()
-    print('Video Downloaded', link)
-
+def download(url):
+    try:
+        video = yt(url)
+    except VideoUnavailable:
+        print(f'Video {url} is unavailable, skipping.')
+        pass
+    else:
+        print(f'Downloading video: {video.title}')
+        video.streams.get_by_itag(22).download()
+        print('Video Downloaded!')
+        subprocess.Popen(r'explorer ' + os.getcwd())
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
